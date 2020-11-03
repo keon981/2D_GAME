48db1dac4d26af0c0b28187a2e16030828880568
keon981 <55180628+keon981@users.noreply.github.com>
2020-11-03 16:02:43 +0800

    碰撞控制

--- 2D-GAME/Assets/Scripts/MenuManager.cs
+++ 2D-GAME/Assets/Scripts/MenuManager.cs
@@ -1,6 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
+      public void StarGame()
+       {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+       }
+
+      public void QuitGame()
+       {
+        print("結束遊戲");
+        Application.Quit();
+       }
+
 }
---
+++ 2D-GAME/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("星之匙");
+        final.SetActive(true);
+    }
+
+}
