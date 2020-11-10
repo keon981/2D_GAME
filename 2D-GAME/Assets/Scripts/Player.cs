67dab4daf00d7fb999ce642c2df0254875348db0
keon981 <55180628+keon981@users.noreply.github.com>
2020-11-10 16:09:41 +0800

    END

--- 2D-GAME/Assets/Scripts/Player.cs
+++ 2D-GAME/Assets/Scripts/Player.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textCount;
+    public int count;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("星之匙");
-        final.SetActive(true);
+        if (collision.name == "門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.tag == "吃貨")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "鑽石數量:" + count;
+        }
+      
+        if (collision.tag == "毒品")
+        {
+            Destroy(gameObject);
+        }
+
     }
 
 }
